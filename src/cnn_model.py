6e7252893c477eaf315cbe8c3a92c2470c6c12e8
Banu-Liyanage <banuka2002liyanage@gmail.com>
2025-10-17 23:01:29 +0530

    updated CNN architecture

--- src/cnn_model.py
+++ src/cnn_model.py
@@ -9,11 +9,11 @@ class SimpleCNN(nn.Module):
     def __init__(self, num_classes=9):
         super(SimpleCNN, self).__init__()
 
-        self.conv_1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, padding=1) # 1st Convolution with 16 filters
-        self.pool_1 = nn.MaxPool2d(kernel_size=2, stride=2) #pooling to reduce dimensionality
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, padding=1) # 1st Convolution with 16 filters
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2) #pooling to reduce dimensionality
 
-        self.conv_2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, padding=1) # 2nd Convolution with 32 filters
-        self.pool_2 = nn.MaxPool2d(kernel_size=2, stride=2) #pooling to reduce dimensionality
+        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, padding=1) # 2nd Convolution with 32 filters
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2) #pooling to reduce dimensionality
 
         self.fc1 = nn.Linear(32 * 16 * 16, 128) # Fully connected layers # 64x64 → 16x16 after 2 poolings
         self.dropout = nn.Dropout(0.3)
